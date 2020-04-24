67909be11b7d4973c8e923ae6fdf6cb2dfc98c8c
yoyopan <yoyo.pan@access-company.com>
2020-04-24 23:15:52 +0800

    [modify] Change port to 8080

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const app = express().use(bodyParser.json()) // creates express http server
 
 // Sets server port and logs message on success
-app.listen(9527, () => console.log('webhook is listening'))
+app.listen(8080, () => console.log('webhook is listening'))
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
